7e5f50dc2efbb7b57bee2fc1dc976347f2d5bb95
Álvaro <alvarocaro99@gmail.com>
2022-05-24 13:44:42 +0200

    Subido script para analisis de la webview

--- automatizacion.sh
+++ automatizacion.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
+/home/alumno/Descargas/sonarqube-9.4.0.54424/bin/linux-x86-64/sonar.sh console &
 
-/home/alumno/Descargas/sonarqube-9.4.0.54424/bin/linux-x86-64/sonar.sh stop &
+sleep 40
+
+cd webview-master
+
+export PATH=$PATH:/home/alumno/Descargas/sonar-scanner-4.7.0.2747-linux/bin
+
+sonar-scanner \
+  -Dsonar.projectKey=Webview \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=http://localhost:9000 \
+  -Dsonar.login=06fb8d3d4340812657286f9fa3ab62c6bd4b17a8 \
+  -Dsonar.java.binaries=gradle -X
